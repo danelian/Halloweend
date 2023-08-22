632fbd000af5bcf0f32779d30aac008122cbdbc6
daneliyan <141317269+danelian@users.noreply.github.com>
2023-08-22 22:54:20 +0300

    Project completion
    Blocks: TrickOrTreat, Subscribe, Footer.

--- halloweend/src/App.vue
+++ halloweend/src/App.vue
@@ -2,15 +2,18 @@
 import Navbar from './components/Navbar.vue';
 import Home from './components/Home.vue';
 import Category from './components/Category.vue';
+import TrickOrTreat from './components/TrickOrTreat.vue';
+import Subscribe from './components/Subscribe.vue';
+import Footer from './components/Footer.vue';
 </script>
 
 <template>
-  <div class="container">
+  <div class="container overflow-x-hidden">
     <Navbar/>
     <Home/>
     <Category/>
+    <TrickOrTreat/>
+    <Subscribe/>
+    <Footer/>
   </div>
 </template>
-
-<style scoped>
-</style>
